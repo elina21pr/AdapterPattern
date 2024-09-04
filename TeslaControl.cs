b0405cf0fbaae5e4ba0cfb8f2293c59350fc2fdc
Elina Prychodko <prychodko2122@gmail.com>
2024-09-04 11:26:56 +0300

    Hot fix

--- TeslaControl.cs
+++ TeslaControl.cs
@@ -11,7 +11,7 @@ namespace FleetManagementFromAdapter
         public void PowerOf() => Console.WriteLine("Power off");
         public void PowerOn() => Console.WriteLine("Power on");
         public void IncreaseSpeed(int kmh) => Console.WriteLine($"Increasing speed by {kmh} km/h");
-        public void UseBrake() => Console.WriteLine($"Using brake", "/n");
+        public void UseBrake() => Console.WriteLine($"Using brake");
 
         public string GetTeslaStatus() => "Tesla is in good condition";
 
